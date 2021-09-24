0bb94e3768886c22f0d42096104cb2a2aed4750a
Carlos Tellez <itconsultor@gmail.com>
2021-09-23 23:02:44 -0500

    Whole project

---
+++ terraform/static-site/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = var.region
+    profile = var.profile
+}
+
+resource "aws_instance" "site" {
+    ami = "ami-09e67e426f25ce0d7"
+    instance_type = "t2.micro"
+    key_name = "devops"
+    vpc_security_group_ids = [ "sg-fbcb0af0", "sg-027b2629ff5351b6a" ]
+    tags = {
+      Name = var.name
+      group = var.name
+    }
+}
