61f8b9cc08930a11692c63780ede0cf71f741ab7
Carlos Tellez <itconsultor@gmail.com>
2021-09-25 00:39:03 -0500

    update main.tf in terraform folder

--- terraform/static-site/main.tf
+++ terraform/static-site/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "site" {
     ami = "ami-09e67e426f25ce0d7"
     instance_type = "t2.micro"
-    key_name = "devops"
+    key_name = "devops_1"
     vpc_security_group_ids = [ "sg-fbcb0af0", "sg-027b2629ff5351b6a" ]
     tags = {
       Name = var.name
